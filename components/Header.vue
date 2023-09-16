d65c928778f28f2f7618457c2b000692e63d3dbe
Jan <58551692+skyZcoding@users.noreply.github.com>
2023-09-16 02:33:34 +0200

    Created Header

---
+++ components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+    <nav class="bg-white shadow-sm">
+        <div class="flex flex-wrap justify-between p-4 items-center">
+            <img src="../logos/MigrosLogo.svg" class="h-10">
+            <div class="flex flex-row items-center">
+                
+            </div>
+        </div>
+    </nav>
+</template>
