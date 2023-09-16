ed780ebba3795c4bfdbb0b488ff61c780dd35fba
Jan <58551692+skyZcoding@users.noreply.github.com>
2023-09-16 04:11:12 +0200

    added qrcode page

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import QRCode from '../views/QRCode.vue'
 
 const routes = [
-    { path: '/', name: 'Home', component: Home }
+    { path: '/', name: 'Home', component: Home },
+    { path: '/qrcode', name: 'QRCode', component: QRCode }
 ]
 
 const router = createRouter({
