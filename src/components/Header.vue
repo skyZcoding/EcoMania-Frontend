353f70cc9b52519dc2d22f264c0b03ca44de2664
Jan <58551692+skyZcoding@users.noreply.github.com>
2023-09-16 09:02:47 +0200

    Added first images

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,24 @@
 <template>
     <nav class="bg-white">
         <div class="flex flex-wrap justify-between p-4 items-center">
-            <img src="../assets/logo.svg" class="h-10">
-            
+            <router-link to="/">
+                <img src="../assets/logo.svg" class="h-10">
+            </router-link>
+            <div class="flex flex-row items-center">
+                    <router-link to="/scanner">
+                        <QRCodeIcon class="text-slate-800 hover:text-[#EE6F2D] duration-200 h-10 mx-2" />
+                    </router-link>
+                </div>
         </div>
     </nav>
 </template>
+
+<script>
+import QRCodeIcon from "./icons/QRCode.vue"
+
+export default {
+    components: {
+        QRCodeIcon
+    },
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,22 @@
 <template>
-    <img src="../assets/icons/Subtract.svg" class="h-auto w-full">
+    <div class="flex flex-col">
+        <div class="h-auto w-auto relative bg-[#A7E7D3]">
+            <img src="../assets/icons/Subtract.svg" class="h-auto w-full">
+            <h1 class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-semibold md:text-5xl text-2xl text-white">Revive Reef</h1>
+        </div>
+        <div class="bg-[#A7E7D3] h-fit w-auto flex flex-col items-center px-24">
+            <button class="mt-24 w-fit h-fit px-24 py-2 rounded-md bg-[#EE6F2D] font-medium text-white">
+                Create a Scrapster!
+            </button>
+            <p class="mt-24 text-center">In the enchanting world of Revive Reef, a mysterious island in the heart of the ocean, a magical transformation occurs. All the trash that washes ashore is not discarded but given a second chance at life. This island is a beacon of hope, where refuse is reborn as lovable and mischievous toy monsters known as</p>
+            <img class="mt-24" src="../assets/icons/scrapters.svg" alt="">
+            <div class="flex flex-row mt-24">
+                <img class="" src="../assets/icons/Monster1.svg" alt="">
+                <img class="" src="../assets/icons/Monster2.svg" alt="">
+                <img class="" src="../assets/icons/Monster3.svg" alt="">
+            </div>
+        </div>
+    </div>
 </template>
 
 <script setup>
