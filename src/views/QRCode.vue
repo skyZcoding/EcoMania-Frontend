a0dc22cbba7a82031c04457a86b60c3b67250354
Jan <58551692+skyZcoding@users.noreply.github.com>
2023-09-16 04:48:18 +0200

    Added QR-Code-Reader

--- src/views/QRCode.vue
+++ src/views/QRCode.vue
@@ -1,9 +1,27 @@
 <template>
-    
+    <div>
+        <QrcodeStream @detect="onDetect">
+        </QrcodeStream>
+    </div>
 </template>
 
 <script>
-export default {
+import { QrcodeStream, QrcodeDropZone, QrcodeCapture } from 'vue-qrcode-reader'
 
+export default {
+    methods: {
+        onDetect(detectedCodes) {
+            console.log("Testing detected")
+            console.log(detectedCodes[0].rawValue)
+        }
+    },
+    data() {
+        
+    },
+    components: {
+        QrcodeStream,
+        QrcodeDropZone,
+        QrcodeCapture
+    },
 }
 </script>
