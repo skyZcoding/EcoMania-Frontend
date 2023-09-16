6dde905226a6911e5eaa2bf05bd9387e98e9d197
Jan <58551692+skyZcoding@users.noreply.github.com>
2023-09-16 11:28:01 +0200

    added another level to landing page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,18 +4,31 @@
             <img src="../assets/icons/Subtract.svg" class="h-auto w-screen">
             <h1 class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-semibold md:text-8xl text-2xl text-white">Revive Reef</h1>
         </div>
-        <div class="bg-[#A7E7D3] h-fit w-screen flex flex-col items-center px-4 md:px-24">
+        <div class="bg-[#A7E7D3] h-fit w-screen flex flex-col items-center px-4 md:px-24 pb-24">
             <button class="mt-24 w-fit h-fit px-24 py-2 rounded-md bg-[#EE6F2D] font-medium text-white">
                 Create a Scrapster!
             </button>
-            <p class="mt-24 text-center">In the enchanting world of Revive Reef, a mysterious island in the heart of the ocean, a magical transformation occurs. All the trash that washes ashore is not discarded but given a second chance at life. This island is a beacon of hope, where refuse is reborn as lovable and mischievous toy monsters known as</p>
+            <p class="mt-24 text-center font-normal">In the enchanting world of Revive Reef, a mysterious island in the heart of the ocean, a magical transformation occurs. All the trash that washes ashore is not discarded but given a second chance at life. This island is a beacon of hope, where refuse is reborn as lovable and mischievous toy monsters known as</p>
             <img class="mt-24" src="../assets/icons/scrapster.svg" alt="">
             <div class="mt-24">
                 <div class="flex flex-row">
                     <img class="" src="../assets/icons/Monster1.svg" alt="">
                 </div>
-
             </div>
+        </div>
+        <div class="md:h-96 h-40 w-screen relative">
+            <img src="../assets/icons/level2.svg" class="h-auto w-screen absolute">
+            <div class="flex flex-col h-full">
+                <div class="h-1/2 bg-[#A7E7D3]"></div>
+                <div class="h-1/2"></div>
+            </div>
+        </div>
+        <div class="mb-24 flex flex-row justify-center items-center px-4 md:px-24 md:mt-56 lg:mt-96 mt-48">
+            <p class="w-2/3 font-normal mr-10">
+                The island has a bustling port: Driftwood Harbor. 
+    Our story follows two siblings, Lily and Finn, who discover this magical island during a family vacation. Lily and Finn stumble upon the ancient secret of awakening the Scrapsters. They craft toys from discarded materials found on the beach and bring them to life using magical Coral Signs (QR codes), a rare and powerful substance unique to Revive Reef.
+            </p>
+            <img src="../assets/icons/kids.svg" class="w-1/3" />
         </div>
     </div>
 </template>
